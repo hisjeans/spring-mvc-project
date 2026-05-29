eb5716db4defa117ec33faba209d88623abbf6b1
hisjeans <his7jeans@gmail.com>
2026-05-29 10:09:25 +0900

    Initial commit

---
+++ src/test/java/com/codeit/mvc/SpringMvcPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeit.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
